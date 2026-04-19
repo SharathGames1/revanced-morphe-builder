0fc1aedd5a6bc2cdef62c0f77a2f5606ace414a5
Sharath <sharathsukash@gmail.com>
2026-04-19 13:37:27 +0530

    Update utils.sh
    Signed-off-by: Sharath <sharathsukash@gmail.com>

--- utils.sh
+++ utils.sh
@@ -318,7 +318,7 @@ merge_splits() {
 		return 1
 	fi
 	# sign the merged stock apk
-	if ! OP=$(java -jar "$APKSIGNER" sign --ks ks-p12.keystore --ks-pass pass:123456789 --key-pass pass:123456789 --ks-key-alias jhc \
+	if ! OP=$(java -jar "$APKSIGNER" sign --ks ks-p12.keystore --ks-pass pass:$KEYSTORE_PASS --key-pass pass:$KEYSTORE_PASS --ks-key-alias $KEYSTORE_ALIAS \
 		--out "${output}" "${output}-unsigned"); then
 		epr "apksigner error: $OP"
 		return 1
